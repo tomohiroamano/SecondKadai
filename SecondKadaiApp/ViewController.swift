fd63f58483dccd8df0494536f0c6dee0210b0ac4
天野智広 <amanotomohiro@amanotomohiro-no-macbook-pro.local>
2019-01-24 23:21:41 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 天野智広 on 2019/01/24.
+//  Copyright © 2019 天野智広. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
